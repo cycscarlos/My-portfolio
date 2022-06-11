f1c75b753f4b772d5bbdd7f5e385aac75c751560
zCoder <cycs.ingenieria@gmail.com>
2022-06-11 19:52:03 -0400

    Cambio de foto en about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import ME from "../../assets/img/me-about.jpg";
+import ME from "../../assets/img/me-about.png";
 import { FaAward } from "react-icons/fa";
 import { HiOutlineUsers } from "react-icons/hi";
 import { VscFolderLibrary } from "react-icons/vsc";
